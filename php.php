a2e5294476582c110e5b9ec6cf5ed7d51f077579
xiang <heroes618@hotmail.com>
2015-01-06 18:44:54 -0800

    property and method

--- php.php
+++ php.php
@@ -13,11 +13,17 @@
 	*/
 	class weather
 	{
-		public $weather1 = "spring";
+		public $weather1 = "spring"; // for every varible you have to use the public, protected, and private keywords.
 		public $weather2 = "summer";
 		public $weather3 = "fall";
 		public $weather4 = "winter";
-	}
+	function theweather() {
+
+return “{$this­>weather1}” .
+
+“{$this­>weather2}”;
+
+}
 
 
 
