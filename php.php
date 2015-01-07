cb52a76d08b730e6add4fb603a39214c23412f4f
xiang <heroes618@hotmail.com>
2015-01-06 20:33:22 -0800

    the final

--- php.php
+++ php.php
@@ -6,30 +6,27 @@
 <body>
 
 <?php 
-	echo "hello";
-
-	/**
-	* 
-	*/
-	class weather
-	{
-		public $weather1 = "spring"; // for every varible you have to use the public, protected, and private keywords.
-		public $weather2 = "summer";
-		public $weather3 = "fall";
-		public $weather4 = "winter";
-	function theweather() {
-
-return “{$this­>weather1}” .
-
-“{$this­>weather2}”;
+	class weather{
+				public $weather1 = "spring"; // for every varible you have to use the public, protected, and private keywords.
+				public $weather2 = "summer";
+				public $weather3 = "fall";
+				public $weather4 = "winter";
+	
+				function theweather() {
+				return “$this­>weather1” .  //any object­by­object basis using the characters ‘­>’ in conjunction with an object variable and property name.
+				“{$this­>weather2}”;
+				}
 
 }
-
 $me = new weather();
 
+$me­> weather1 = “summber”;
 
+$me­> weather4 = “winter”;
 
-	//any object­by­object basis using the characters ‘­>’ in conjunction with an object variable and property name.
+print “The weather is {$me­>theweather()}.”;
+
+	
 
 
 
