9f362f1cf98b2e38f89492f6562c2058bb8aa812
xiang <heroes618@hotmail.com>
2015-01-08 09:20:31 -0800

    string

---
+++ run.php
@@ -0,0 +1,35 @@
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+
+/**
+* 
+*/
+
+
+if(is_string("hello")){
+
+echo "hello";
+}
+else
+{
+echo "boom";
+
+var_dump(is_string("XVY"));
+
+var_dump(is_string("8008"));
+
+var_dump(is_string("123"));
+
+var_dump(is_string("ture"));
+}
+
+?>
+
+</body>
+</html>
