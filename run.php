5a07b9a04ec3009952c93e60d19c843c41319948
xiang <heroes618@hotmail.com>
2015-01-08 18:53:01 -0800

    if and else part 2, like the twilight

--- run.php
+++ run.php
@@ -12,13 +12,13 @@
 */
 
 //====example 1====//
-if(is_string("hello")){   // 
+if(is_string("hello")){   // the frist string only echos out the if statment.
 
-echo "hello";
+echo "hello and good day";
 }
 else
 {
-echo "boom";
+echo "hello my name is xiang"; // thats because the bracket is preven the if and else togeter, which causes display only one echo  
 
 var_dump(is_string("XVY"));
 
@@ -26,12 +26,12 @@ var_dump(is_string("8008"));
 
 var_dump(is_string("123"));
 
-var_dump(is_string("ture"));
+var_dump(is_string("false"));
 }
 
 //====example 2====//
 
-if (is_string("string"))
+if (is_string("string")) // second example. This example shows what happen if you dont have bracket.
 
 echo "It is a string";
 
@@ -39,13 +39,35 @@ else
 
 echo "it is a string";
 
-var_dump(is_string("XYZ"));
+var_dump(is_string("a string are character data"));
 
 var_dump(is_string("7777"));
 
 var_dump(is_string("123"));
 
-var_dump(is_string("false"));
+var_dump(is_string("true"));
+
+//=====example 3====//
+
+
+
+if (is_string(":) hello") // 
+
+echo ":)";
+
+else
+
+echo ":(";
+
+var_dump(is_string("a string are character data"));
+
+var_dump(is_string("7777"));
+
+var_dump(is_string("123"));
+
+var_dump(is_string("true"));
+
+
 
 ?>
 
