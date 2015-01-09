5d7cd5afa549b14f355f0faef5ae74e53d2fd8ed
xiang <heroes618@hotmail.com>
2015-01-08 18:18:16 -0800

    if and else

--- run.php
+++ run.php
@@ -11,8 +11,8 @@
 * 
 */
 
-
-if(is_string("hello")){
+//====example 1====//
+if(is_string("hello")){   // 
 
 echo "hello";
 }
@@ -28,6 +28,24 @@ var_dump(is_string("123"));
 
 var_dump(is_string("ture"));
 }
+
+//====example 2====//
+
+if (is_string("string"))
+
+echo "It is a string";
+
+else
+
+echo "it is a string";
+
+var_dump(is_string("XYZ"));
+
+var_dump(is_string("7777"));
+
+var_dump(is_string("123"));
+
+var_dump(is_string("false"));
 
 ?>
 
