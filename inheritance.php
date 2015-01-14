9f62205130c97915648a0ebdd146f946d9c605e2
xiang <heroes618@hotmail.com>
2015-01-13 21:35:20 -0800

    still working on the class and extended class

--- inheritance.php
+++ inheritance.php
@@ -24,8 +24,100 @@
 
 
 		}
+
+		function Mymusic(){
+			return "my favor type of music is" .$this->type "and the name is". $this->name;
+
+		}
+
 	}
 
+	class pop extends music{ // you can created a child class, that can act as extends the class
+		function yellow(){
+			return $this->coldplay;
+		}
+
+	}
+
+	class rock extends music{
+		function highway(){
+			return $this->hellye;
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class fish {
+	public $breed;
+	public $type;
+	public $name;
+	public $size;
+	public $where;
+	public $eat;
+	public $lifeline;
+
+function __construct($type, $breed, $name, $size, $where, $eat, $lifeline) {
+	$this->breed = $breed;
+	$this->type = $type;
+	$this->name = $name;
+	$this->size = $size;
+	$this->where = $where;
+	$this->eat = $eat;
+	$this->lifeline = $lifeline;
+
+	
+
+}
+
+function fishing() {
+
+return “This is my “ . $this­>firstName .
+
+}
+
+}
+
+class Cat extends Animal {
+
+function greet() {
+
+return $this­>meow”;
+
+}
+
+“ and last ” . $this­>lastName;
+
+}
+
+class Dog extends Animal {
+
+function hello() {
+
+return $this­>bark;
+
+}
+
+}
+$cat = new Cat(“Cat”, “Mellow”, “Yellow”, “male”, 15);
+
+print “Animal 1 is a ” . $cat­>getName();
 ?>
 
 </body>
