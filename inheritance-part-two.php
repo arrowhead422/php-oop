19508ad43488e99cf72d92359f1b1444f6843049
xiang <heroes618@hotmail.com>
2015-01-14 22:15:27 -0800

    almost finish

--- inheritance-part-two.php
+++ inheritance-part-two.php
@@ -6,6 +6,9 @@
 <body>
 
 <?php 
+
+//========example 1 =============
+
 	class mankind{
 		public $woman;
 		public $man;
@@ -24,7 +27,7 @@
 	}
 
 	function HowBasic(){
-		return "mankind first stared they alreally created the".$this->law. "and" $this->philosophy. ":)";
+		return "mankind first stared they alreally created the". $this->law. "and" $this->philosophy. ":)";
 	}
 
 	}
@@ -32,18 +35,79 @@
 	class job extends mankind{
 		function __construct($woman, $man, $child, $techology, $philosophy, $law){
 			parent::__construct($woman, $man, $child, $techology, $philosophy, $law);
-			$this->meow = $meow;
+			$this->law = $law;
 		}
 		function meet(){
-			return $this->meow;
+			return $this->law;
 
 		}
 
 	}
 
 	class life extends animal{
-		function __contruct()
+		function __contruct($woman, $man, $child, $techology, $philosophy, $law)
+		parent::__construct($woman, $man, $child, $techology, $philosophy, $law);
+			$this->philosophy = $philosophy;
+
 	}
+	function intent(){
+		return $this->philosophy;
+	}
+
+	$job = new job("man", "race", "million", "smart", 72, true);
+	print "every human can and have". $cat->HowBasic();
+
+
+
+//======example 2 =========
+
+	class music {
+		public $type;
+		public $year;
+		public $time;
+		public $player;
+		public $name;
+		public $quality
+		
+		function hipster($type, $year, $time, $player, $name, $quality)
+		{
+			$this->type = $type;//The $this is a mechanism by which a class can refer to an object instance.
+			$this->year = $year;
+			$this->time = $time;
+			$this->player = $player;
+			$this->name = $name;
+			$this->quality = $quality;
+
+
+
+		}
+
+		function Mymusic(){
+			return "my favor type of music is" .$this->type "and the name is". $this->name;
+
+		}
+
+	}
+
+	class coldplay extends music{ // you can created a child class, that can act as extends the class
+		function yellow(){
+			return $this->name;
+		}
+
+	}
+
+	class icebaby extends music{
+		function baby(){
+			return $this->quality;
+		}
+
+	}
+	$pop = new pop("when" "fame" "play");
+
+print "this music has " . $pop­>Mymusic();
+
+
+
 
 ?>
 
